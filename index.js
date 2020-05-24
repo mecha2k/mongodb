5876479807f502d9fe35e473527c7b24cf3b777b
mecha2k <mecha2k@naver.com>
2020-05-24 18:16:30 +0900

    1st commit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose")
 const dotenv = require("dotenv")
 
-console.log(app.get("env"))
+const app = require("./app")
+
 dotenv.config({ path: "./.env" })
 
-const DB = process.env["DATABASE"].replace("<PASSWORD>", process.env["DATABASE_PASSWORD"])
-const localDB = process.env["DATABASE_LOCAL"]
+const DB = process.env.DATABASE_LOCAL
 mongoose
   .connect(DB, {
     useNewUrlParser: true,
@@ -14,6 +14,41 @@ mongoose
     useUnifiedTopology: true
   })
   .then((connect) => {
-    console.log(connect.connection)
+    // console.log(connect.connection)
     console.log("Database connection successful.")
   })
+
+const personschema = mongoose.Schema({
+  name: { type: String, unique: true, required: [true, "A name must be provided"] },
+  age: Number
+  // stories: [{ type: mongoose.Schema.ObjectId, ref: "Story" }]
+})
+
+const storyschema = mongoose.Schema({
+  // author: { type: mongoose.Schema.ObjectId, ref: "Person" },
+  title: { type: String, required: [true, "A title must be provided"] }
+  // fans: [{ type: mongoose.Schema.ObjectId, ref: "Person" }]
+})
+
+const Story = mongoose.model("stories", storyschema)
+const User = mongoose.model("users", personschema)
+
+const author = new User({ name: "Ian Fleming", age: 50 })
+
+// Person.create(author)
+//   .then((doc) => console.log(doc))
+//   .catch((err) => console.log(err))
+
+author
+  .save()
+  .then((doc) => console.log(doc))
+  .catch((err) => console.log(err))
+
+// const story1 = new Story({
+//   title: "Casino Royale",
+//   author: author._id
+// })
+//
+// story1.save(function(err) {
+//   if (err) return console.log(err)
+// })
